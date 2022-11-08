5012bba9439c991cc57ff834aeb5256c7117083b
Eren Ozcan <erenozcandev@gmail.com>
2022-11-09 00:00:57 +0300

    heroSection update

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,16 +1,16 @@
 export default function HeroSection() {
-  return (
-    <div className="container min-w-full">
-      <div className="">
+  return(
+    <div className="container mx-auto">
+      <div className="flex flex-col md:flex-row items-center justify-center text-center">
         <div className="pt-5">
-          <div className="flex justify-center">
-            <img className="h-52 w-52 rounded-lg mb-4" src="https://i.hizliresim.com/ls7qonb.jpeg" alt="img"/>
-          </div>
+          <img className="w-50 h-60 rounded-lg" src="https://i.hizliresim.com/logetgk.png" alt="img"/>
+        </div>
+        <div className="pt-12">
           <div className="animate-fade-in-down mb-1 text-center text-xl font-bold dark:text-gray-300">Eren Ozcan</div>
           <div className="animate-fade-in-down mb-8 text-center text-lg dark:text-gray-300">Front-End Developer</div>
           <blockquote>
-            <p className="animate-fade-in-down mx-12 mb-9 text-center text-lg dark:text-gray-300">
-              Hey there, I am Eren. I enjoy coding. I am constantly improving my coding skills.
+            <p className="animate-fade-in-down mx-12 mb-9 md:pl-10 text-center text-lg dark:text-gray-300">
+              Hey there, I am Eren. I enjoy coding. <br /> I am constantly improving my coding skills.
             </p>
           </blockquote>
         </div>
