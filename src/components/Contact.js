ba9b681c16ca0b732cefde4f5cb325cac6256a58
eren ozcan <erenozcandev@gmail.com>
2023-05-10 13:39:23 +0300

    edit mail address

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,7 @@
 export default function Contact() {
   return(
     <div className="flex justify-center mx-12 bg-gray-200 dark:bg-[#202125] sm:rounded-lg text-center pb-10">
-      <h1 className="font-bold text-gray-700 dark:text-gray-400 text-xl">info@erenozcan.com</h1>
+      <h1 className="font-bold text-gray-700 dark:text-gray-400 text-xl">erenozcandev@gmail.com</h1>
     </div>
   )
 }
