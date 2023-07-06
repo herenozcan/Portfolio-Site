cbabc86806f3ebc8aa8ed94f14e03136d3ed694d
ozcaneren <100240225+ozcaneren@users.noreply.github.com>
2023-07-06 13:11:02 +0300

    Update HeroSection.js

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -15,7 +15,7 @@ export default function HeroSection() {
         </div>
         <div className="pt-12">
           <div className="animate-fade-in-down mb-1 text-center text-xl font-bold dark:text-gray-300 md:ml-12">Eren Ozcan</div>
-          <div className="animate-fade-in-down mb-8 text-center text-lg dark:text-gray-300 md:ml-12">{t('Full Stack Web Developer')}</div>
+          <div className="animate-fade-in-down mb-8 text-center text-lg dark:text-gray-300 md:ml-12">{t('Frontend Web Developer')}</div>
           <blockquote>
             <p className="animate-fade-in-down mx-12 mb-9 md:pl-10 text-center text-lg dark:text-gray-300">
             {t('Hey there, I am Eren. I enjoy coding.')} <br /> {t('I am constantly improving my coding skills.')}
