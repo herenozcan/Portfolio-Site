c480d8cf839c21ed52946297eb269452c47ee2f2
eren ozcan <erenozcandev@gmail.com>
2023-08-11 18:33:17 +0300

    coming soon :)))

---
+++ src/pages/cards/bookmarkapp.js
@@ -0,0 +1,13 @@
+import React from "react";
+//import Bookmark from "../images/bookmark1.png";
+
+
+function bookmarkapp() {
+  return (
+    <>
+      
+    </>
+  );
+}
+
+export default bookmarkapp;
