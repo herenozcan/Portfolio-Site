48a5846f55f842d201cdc1acfafef66d44b6dc83
eren ozcan <erenozcandev@gmail.com>
2023-08-11 18:16:45 +0300

    projects page loading 1/2

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,13 +1,13 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { useTranslation } from 'react-i18next';
-import darkLight from '../images/darklightmode.jpg';
-import fakeStore from '../images/fakestore.png';
-import cssNesting from '../images/css-nesting.png';
+import { useTranslation } from "react-i18next";
+import darkLight from "../images/darklightmode.jpg";
+import fakeStore from "../images/fakestore.png";
+import cssNesting from "../images/css-nesting.png";
+import Bookmark from "../images/bookmark1.png";
 
 export default function Projects() {
-
   const settings = {
     dots: false,
     infinite: true,
@@ -22,105 +22,195 @@ export default function Projects() {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 2
-        }
+          slidesToShow: 2,
+        },
       },
       {
         breakpoint: 640,
         settings: {
-          slidesToShow: 1
-        }
-      }
-    ]
+          slidesToShow: 1,
+        },
+      },
+    ],
   };
 
   const { t } = useTranslation();
 
   return (
     <div className="mx-12 text-center pt-9 pb-9">
-      <h1 className="text-xl font-semibold pb-4 text-gray-700 dark:text-gray-400">{t('Projects')}</h1>
+      <h1 className="text-xl font-semibold pb-4 text-gray-700 dark:text-gray-400">
+        {t("Projects")}
+      </h1>
       <Slider {...settings}>
         <div className="w-full p-4">
           <div className="block h-30 overflow-hidden rounded-lg bg-[#5D3EBC]">
-            <img alt="ecommerce" className="block h-[260px] w-full object-cover object-center cursor-pointer" src="https://cdn.getir.com/marketing/Getir_Logo_1621812382342.png" />
+            <img
+              alt="ecommerce"
+              className="block h-[260px] w-full object-cover object-center cursor-pointer"
+              src="https://cdn.getir.com/marketing/Getir_Logo_1621812382342.png"
+            />
           </div>
           <div className="mt-4 text-center">
-            <h3 className="title-font mb-1 text-xs tracking-widest dark:text-gray-200 text-gray-500">{t('PROJECT')}</h3>
-            <a className="title-font text-lg font-medium text-gray-900 dark:text-gray-300" href="https://github.com/ozcaneren/Getir-React-Duplicate">{t('Getir Website Clone')}</a>
+            <h3 className="title-font mb-1 text-xs tracking-widest dark:text-gray-200 text-gray-500">
+              {t("PROJECT")}
+            </h3>
+            <a
+              className="title-font text-lg font-medium text-gray-900 dark:text-gray-300"
+              href="https://github.com/ozcaneren/Getir-React-Duplicate"
+            >
+              {t("Getir Website Clone")}
+            </a>
             <p className="mt-1 dark:text-gray-400">20/10/2022</p>
           </div>
         </div>
         <div className="w-full p-4">
           <div className="block h-30 overflow-hidden rounded-lg">
-            <img alt="ecommerce" className="block h-[260px] w-full object-cover object-center cursor-pointer rounded-md" src="https://studio.uxpincdn.com/studio/wp-content/uploads/2021/05/Want-to-Convert-Design-To-Code_-Heres-A-Better-Way.png.webp" />
+            <img
+              alt="ecommerce"
+              className="block h-[260px] w-full object-contain bg-[#6EA1B0] object-center cursor-pointer rounded-md"
+              src={Bookmark}
+            />
           </div>
           <div className="mt-4 text-center">
-            <h3 className="title-font mb-1 text-xs tracking-widest text-gray-500 dark:text-gray-200">{t('PROJECT')}</h3>
-            <a className="title-font text-lg font-medium text-gray-900 dark:text-gray-300"  href="https://github.com/ozcaneren/HTML-CSS-Designs">{t('HTML/CSS Designs')}</a>
-            <p className="mt-1 dark:text-gray-400">17/08/2022</p>
+            <h3 className="title-font mb-1 text-xs tracking-widest text-gray-500 dark:text-gray-200">
+              {t("PROJECT")}
+            </h3>
+            <a
+              className="title-font text-lg font-medium text-gray-900 dark:text-gray-300"
+              href="https://github.com/ozcaneren/HTML-CSS-Designs"
+            >
+              {t("Bookmark App")}
+            </a>
+            <p className="mt-1 dark:text-gray-400">03/08/2023</p>
           </div>
         </div>
         <div className="w-full p-4">
           <div className="block h-30 overflow-hidden rounded-lg">
-            <img alt="ecommerce" className="block h-[260px] w-full object-cover object-center cursor-pointer" src="https://wallpaperbat.com/img/193513-python-wallpaper-awesome-55-programming-hd-wallpaper-python.jpg" />
+            <img
+              alt="ecommerce"
+              className="block h-[260px] w-full object-cover object-center cursor-pointer"
+              src="https://wallpaperbat.com/img/193513-python-wallpaper-awesome-55-programming-hd-wallpaper-python.jpg"
+            />
           </div>
           <div className="mt-4 text-center">
-            <h3 className="title-font mb-1 text-xs tracking-widest text-gray-500 dark:text-gray-200">{t('PROJECT')}</h3>
-            <a className="title-font text-lg font-medium text-gray-900 dark:text-gray-300"  href="https://github.com/ozcaneren/Python-Projects">{t('Python Projects')}</a>
+            <h3 className="title-font mb-1 text-xs tracking-widest text-gray-500 dark:text-gray-200">
+              {t("PROJECT")}
+            </h3>
+            <a
+              className="title-font text-lg font-medium text-gray-900 dark:text-gray-300"
+              href="https://github.com/ozcaneren/Python-Projects"
+            >
+              {t("Python Projects")}
+            </a>
             <p className="mt-1 dark:text-gray-400">12/11/2021</p>
           </div>
         </div>
         <div className="w-full p-4">
           <div className="block h-30 overflow-hidden rounded-lg">
-            <img alt="ecommerce" className="block h-[260px] w-full object-cover object-center cursor-pointer" src="https://www.pixelstalk.net/wp-content/uploads/images6/SpaceX-Wallpaper-HD-Free-download.jpg" />
+            <img
+              alt="ecommerce"
+              className="block h-[260px] w-full object-cover object-center cursor-pointer"
+              src="https://www.pixelstalk.net/wp-content/uploads/images6/SpaceX-Wallpaper-HD-Free-download.jpg"
+            />
           </div>
           <div className="mt-4 text-center">
-            <h3 className="title-font mb-1 text-xs tracking-widest text-gray-500 dark:text-gray-200">{t('PROJECT')}</h3>
-            <a className="title-font text-lg font-medium text-gray-900 dark:text-gray-300"  href="https://github.com/ozcaneren/SpaceX-Clone">{t('SpaceX Clone')}</a>
+            <h3 className="title-font mb-1 text-xs tracking-widest text-gray-500 dark:text-gray-200">
+              {t("PROJECT")}
+            </h3>
+            <a
+              className="title-font text-lg font-medium text-gray-900 dark:text-gray-300"
+              href="https://github.com/ozcaneren/SpaceX-Clone"
+            >
+              {t("SpaceX Clone")}
+            </a>
             <p className="mt-1 dark:text-gray-400">28/08/2022</p>
           </div>
         </div>
         <div className="w-full p-4">
           <div className="block h-30 overflow-hidden rounded-lg">
-            <img alt="ecommerce" className="block h-[260px] w-full object-cover object-center cursor-pointer bg-yellow-500" src="https://lemonkingdom.netlify.app/static/media/lemon720x600.e0f6a5c2491ce2927d47.png" />
+            <img
+              alt="ecommerce"
+              className="block h-[260px] w-full object-cover object-center cursor-pointer bg-yellow-500"
+              src="https://lemonkingdom.netlify.app/static/media/lemon720x600.e0f6a5c2491ce2927d47.png"
+            />
           </div>
           <div className="mt-4 text-center">
-            <h3 className="title-font mb-1 text-xs tracking-widest text-gray-500 dark:text-gray-200">{t('PROJECT')}</h3>
-            <a className="title-font text-lg font-medium text-gray-900 dark:text-gray-300"  href="https://github.com/ozcaneren/SpaceX-Clone">{t('Lemon Kingdom')}</a>
+            <h3 className="title-font mb-1 text-xs tracking-widest text-gray-500 dark:text-gray-200">
+              {t("PROJECT")}
+            </h3>
+            <a
+              className="title-font text-lg font-medium text-gray-900 dark:text-gray-300"
+              href="https://github.com/ozcaneren/SpaceX-Clone"
+            >
+              {t("Lemon Kingdom")}
+            </a>
             <p className="mt-1 dark:text-gray-400">12/12/2022</p>
           </div>
         </div>
         <div className="w-full p-4">
           <div className="block h-30 overflow-hidden rounded-lg">
-            <img alt="ecommerce" className="block h-[260px] w-full object-cover object-center cursor-pointer" src={darkLight} />
+            <img
+              alt="ecommerce"
+              className="block h-[260px] w-full object-cover object-center cursor-pointer"
+              src={darkLight}
+            />
           </div>
           <div className="mt-4 text-center">
-            <h3 className="title-font mb-1 text-xs tracking-widest text-gray-500 dark:text-gray-200">{t('PROJECT')}</h3>
-            <a className="title-font text-lg font-medium text-gray-900 dark:text-gray-300"  href="https://github.com/ozcaneren/react-toggle-dark-mode">{t('Toggle Dark Light Mode')}</a>
+            <h3 className="title-font mb-1 text-xs tracking-widest text-gray-500 dark:text-gray-200">
+              {t("PROJECT")}
+            </h3>
+            <a
+              className="title-font text-lg font-medium text-gray-900 dark:text-gray-300"
+              href="https://github.com/ozcaneren/react-toggle-dark-mode"
+            >
+              {t("Toggle Dark Light Mode")}
+            </a>
             <p className="mt-1 dark:text-gray-400">27/05/2023</p>
           </div>
         </div>
         <div className="w-full p-4">
           <div className="block h-30 overflow-hidden rounded-lg">
-            <img alt="ecommerce" className="block h-[260px] w-full object-cover object-center cursor-pointer" src={fakeStore} />
+            <img
+              alt="ecommerce"
+              className="block h-[260px] w-full object-cover object-center cursor-pointer"
+              src={fakeStore}
+            />
           </div>
           <div className="mt-4 text-center">
-            <h3 className="title-font mb-1 text-xs tracking-widest text-gray-500 dark:text-gray-200">{t('PROJECT')}</h3>
-            <a className="title-font text-lg font-medium text-gray-900 dark:text-gray-300"  href="https://github.com/ozcaneren/fake-store-api">{t('Fake Store API')}</a>
+            <h3 className="title-font mb-1 text-xs tracking-widest text-gray-500 dark:text-gray-200">
+              {t("PROJECT")}
+            </h3>
+            <a
+              className="title-font text-lg font-medium text-gray-900 dark:text-gray-300"
+              href="https://github.com/ozcaneren/fake-store-api"
+            >
+              {t("Fake Store API")}
+            </a>
             <p className="mt-1 dark:text-gray-400">25/05/2023</p>
           </div>
         </div>
         <div className="w-full p-4">
           <div className="block h-30 overflow-hidden rounded-lg">
-            <img alt="ecommerce" className="block h-[260px] w-full object-cover object-center cursor-pointer" src={cssNesting} />
+            <img
+              alt="ecommerce"
+              className="block h-[260px] w-full object-cover object-center cursor-pointer"
+              src={cssNesting}
+            />
           </div>
           <div className="mt-4 text-center">
-            <h3 className="title-font mb-1 text-xs tracking-widest text-gray-500 dark:text-gray-200">{t('PROJECT')}</h3>
-            <a className="title-font text-lg font-medium text-gray-900 dark:text-gray-300"  href="https://github.com/ozcaneren/css-nesting">{t('CSS Nesting')}</a>
+            <h3 className="title-font mb-1 text-xs tracking-widest text-gray-500 dark:text-gray-200">
+              {t("PROJECT")}
+            </h3>
+            <a
+              className="title-font text-lg font-medium text-gray-900 dark:text-gray-300"
+              href="https://github.com/ozcaneren/css-nesting"
+            >
+              {t("CSS Nesting")}
+            </a>
             <p className="mt-1 dark:text-gray-400">24/05/2023</p>
           </div>
         </div>
       </Slider>
-    </div>  
-  )
+    </div>
+  );
 }
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,30 +2,199 @@ import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Divide from "../components/Divide";
+import { useTranslation } from "react-i18next";
+import Bookmark from "../images/bookmark1.png";
+import { BiRightArrowAlt } from "react-icons/bi";
+import Chatapp from "../images/chatapp.jpg";
+import Todo from "../images/to-do-list-apps.png";
 
 function Projects() {
+  const { t } = useTranslation();
+
   return (
     <>
       <div className="bg-gray-200 dark:bg-[#202125]">
         <div className="container mx-auto">
           <Header />
-          <div className="pt-5">
-            <h1 className="text-4xl font-bold text-center text-gray-800 dark:text-white">
-              Projects
+          <div className="mt-8">
+            <h1 className="text-center">
+              <span className="text-4xl font-bold text-gray-800 dark:text-white">
+                {t("Projects")}
+              </span>
             </h1>
           </div>
-          <div className="pt-5">
-            <Divide />
+          <div className="flex justify-center items-center">
+            <div className="grid grid-cols-3 gap-16 text-center pt-9 pb-9">
+              <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <div>
+                  <img
+                    className="rounded-t-lg object-fill bg-[#6EA1B0]"
+                    src={Bookmark}
+                    alt=""
+                  />
+                </div>
+                <div className="p-5">
+                  <div>
+                    <h5 className="mb-2 mt-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                      {t("Bookmark App")}
+                    </h5>
+                  </div>
+                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    {t("A bookmark app that allows you to save your favorite.")}
+                  </p>
+                  <a
+                    href="https://github.com/ozcaneren/bookmark-app"
+                    className="inline-flex items-center mt-14 px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    {t("Go to Project")}
+                    <BiRightArrowAlt size={20} className="ml-2" />
+                  </a>
+                </div>
+              </div>
+              <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <div>
+                  <img
+                    className="rounded-t-lg object-fill bg-[#6EA1B0]"
+                    src={Chatapp}
+                    alt=""
+                  />
+                </div>
+                <div className="p-5">
+                  <div>
+                    <h5 className="mb-2 text-2xl mt-2 font-bold tracking-tight text-gray-900 dark:text-white">
+                      {t("Real Time Chat App")}
+                    </h5>
+                  </div>
+                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    {t(
+                      "A real time chat app that allows you to chat with your."
+                    )}
+                  </p>
+                  <a
+                    href="https://github.com/ozcaneren/real-time-chat-app"
+                    className="inline-flex items-center px-3 py-2 mt-14 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    {t("Go to Project")}
+                    <BiRightArrowAlt size={20} className="ml-2" />
+                  </a>
+                </div>
+              </div>
+              <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <div>
+                  <img
+                    className="rounded-t-lg object-fill w-[382px] h-[382px] bg-[#6EA1B0]"
+                    src={Todo}
+                    alt=""
+                  />
+                </div>
+                <div className="p-5">
+                  <div>
+                    <h5 className="mb-2 text-2xl mt-2 font-bold tracking-tight text-gray-900 dark:text-white">
+                      {t("Todo List App")}
+                    </h5>
+                  </div>
+                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    {t(
+                      "A todo list app that allows you to save your daily tasks."
+                    )}
+                  </p>
+                  <a
+                    href="https://github.com/ozcaneren/todo-with-react-firebase"
+                    className="inline-flex items-center mt-14 px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    {t("Go to Project")}
+                    <BiRightArrowAlt size={20} className="ml-2" />
+                  </a>
+                </div>
+              </div>
+              <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <div>
+                  <img
+                    className="rounded-t-lg object-fill w-[382px] h-[382px] bg-[#6EA1B0]"
+                    src={Todo}
+                    alt=""
+                  />
+                </div>
+                <div className="p-5">
+                  <div>
+                    <h5 className="mb-2 text-2xl mt-2 font-bold tracking-tight text-gray-900 dark:text-white">
+                      {t("Todo List App")}
+                    </h5>
+                  </div>
+                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    {t(
+                      "A todo list app that allows you to save your daily tasks."
+                    )}
+                  </p>
+                  <a
+                    href="https://github.com/ozcaneren/todo-with-react-firebase"
+                    className="inline-flex items-center mt-14 px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    {t("Go to Project")}
+                    <BiRightArrowAlt size={20} className="ml-2" />
+                  </a>
+                </div>
+              </div>
+              <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <div>
+                  <img
+                    className="rounded-t-lg object-fill w-[382px] h-[382px] bg-[#6EA1B0]"
+                    src={Todo}
+                    alt=""
+                  />
+                </div>
+                <div className="p-5">
+                  <div>
+                    <h5 className="mb-2 text-2xl mt-2 font-bold tracking-tight text-gray-900 dark:text-white">
+                      {t("Todo List App")}
+                    </h5>
+                  </div>
+                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    {t(
+                      "A todo list app that allows you to save your daily tasks."
+                    )}
+                  </p>
+                  <a
+                    href="https://github.com/ozcaneren/todo-with-react-firebase"
+                    className="inline-flex items-center mt-14 px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    {t("Go to Project")}
+                    <BiRightArrowAlt size={20} className="ml-2" />
+                  </a>
+                </div>
+              </div>
+              <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <div>
+                  <img
+                    className="rounded-t-lg object-fill w-[382px] h-[382px] bg-[#6EA1B0]"
+                    src={Todo}
+                    alt=""
+                  />
+                </div>
+                <div className="p-5">
+                  <div>
+                    <h5 className="mb-2 text-2xl mt-2 font-bold tracking-tight text-gray-900 dark:text-white">
+                      {t("Todo List App")}
+                    </h5>
+                  </div>
+                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    {t(
+                      "A todo list app that allows you to save your daily tasks."
+                    )}
+                  </p>
+                  <a
+                    href="https://github.com/ozcaneren/todo-with-react-firebase"
+                    className="inline-flex items-center mt-14 px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    {t("Go to Project")}
+                    <BiRightArrowAlt size={20} className="ml-2" />
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="py-56">
-            <h1 className="text-2xl font-bold text-center text-gray-800 dark:text-white">
-              Coming Soon
-            </h1>
-          </div>
-          <div className="pb-10">
-            <Divide />
-          </div>
-          <Footer />
+          <Divide />
+          <Footer className=" fixed bottom-0 left-0 w-full bg-gray-200 dark:bg-[#202125] p-10 text-center" />
         </div>
       </div>
     </>
